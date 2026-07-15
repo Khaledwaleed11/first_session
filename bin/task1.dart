83d14cbf78186526a176510f91b60341e67b322a
Khaled Waled <khaledwaled2003830@gmail.com>
2026-07-16 00:11:46 +0300

    First commit

---
+++ bin/task1.dart
@@ -0,0 +1,9 @@
+bool isLeapYear (int year){
+  return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+}
+void main(){
+  print(isLeapYear(2024));
+  print(isLeapYear(2044));
+  print(isLeapYear(1887));
+  print(isLeapYear(6756));
+}
