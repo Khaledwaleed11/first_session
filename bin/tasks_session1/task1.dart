dee9f75ea1064b22eeddba8e6cbce4363231bf55
Khaled Waled <khaledwaled2003830@gmail.com>
2026-07-16 04:23:13 +0300

    task1

---
+++ bin/tasks_session1/task1.dart
@@ -0,0 +1,12 @@
+void main() {
+  String name = "khaled waled";
+  int age = 23;
+  String country = "Egypt";
+  String favouriteColour = "Black";
+  print(
+    "my name is $name\n"
+    "my age is $age\n"
+    "I am from $country\n"
+    "My favourite color is $favouriteColour",
+  );
+}
