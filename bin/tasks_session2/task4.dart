c034cedb0c51a0a24bd78ac1a31200da938acaca
Khaled Waled <khaledwaled2003830@gmail.com>
2026-07-16 03:56:15 +0300

    task4

--- bin/tasks_session2/task4.dart
+++ bin/tasks_session2/task4.dart
@@ -1,6 +1,7 @@
-List reverseTwoNumbers (int num1 , int num2){
-  return [num2,num1];
+List reverseTwoNumbers(int num1, int num2) {
+  return [num2, num1];
 }
-void main(){
-print(reverseTwoNumbers(7,9 ));
+
+void main() {
+  print(reverseTwoNumbers(7, 9));
 }
