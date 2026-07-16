232ea44882dd46fc704cbf4aa788b50c03dc2920
Khaled Waled <khaledwaled2003830@gmail.com>
2026-07-16 04:36:03 +0300

    task4

---
+++ bin/tasks_session1/task4.dart
@@ -0,0 +1,12 @@
+void main() {
+  double width = 8;
+  double height = 5;
+  double area = width * height;
+  double perimeter = (width + height) * 2;
+  print(
+    "Width = $width\n"
+    "Height = $height\n"
+    "Area = $area\n"
+    "Perimeter = $perimeter",
+  );
+}
