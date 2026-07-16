4418b95a4996af1f6e89dbcff66141b7ba760364
Khaled Waled <khaledwaled2003830@gmail.com>
2026-07-16 03:55:14 +0300

    task3

--- bin/tasks_session2/task3.dart
+++ bin/tasks_session2/task3.dart
@@ -1,13 +1,14 @@
-int getPositiveNumber(List<int>numbers){
+int getPositiveNumber(List<int> numbers) {
   int count = 0;
-  for(int i = 0; i<numbers.length; i++){
-    if(numbers[i]>0){
+  for (int i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
       count++;
     }
   }
   return count;
 }
-void main (){
-  List<int>numbers = [3,4,5,6,-4,-8];
+
+void main() {
+  List<int> numbers = [3, 4, 5, 6, -4, -8];
   print(getPositiveNumber(numbers));
 }
