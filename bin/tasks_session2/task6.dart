e7309ffd913d5087a6878f531f8f706ca15277d9
Khaled Waled <khaledwaled2003830@gmail.com>
2026-07-16 03:58:04 +0300

    task6

--- bin/tasks_session2/task6.dart
+++ bin/tasks_session2/task6.dart
@@ -1,12 +1,13 @@
-int getMissingNumber(List<int>numbers){
-  int n = numbers.length+1;
-  int total = n*(n+1)~/2;
-  int sum = 0 ;
-  for(int i =0;i<numbers.length;i++){
-    sum +=numbers[i];
+int getMissingNumber(List<int> numbers) {
+  int n = numbers.length + 1;
+  int total = n * (n + 1) ~/ 2;
+  int sum = 0;
+  for (int i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
   }
-  return total-sum;
+  return total - sum;
 }
-void main (){
-  print(getMissingNumber([1,2,3,4,6,7,8,9,10]));
+
+void main() {
+  print(getMissingNumber([1, 2, 3, 4, 6, 7, 8, 9, 10]));
 }
