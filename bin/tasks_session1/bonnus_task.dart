b8b2df2c13f25c6ca213c4529b9c4da4c48a5bea
Khaled Waled <khaledwaled2003830@gmail.com>
2026-07-16 04:50:43 +0300

    Bonus_task

---
+++ bin/tasks_session1/bonnus_task.dart
@@ -0,0 +1,14 @@
+void main() {
+  String title = "The Bee Keeper";
+  int year = 2025;
+  double rate = 9.5;
+  String genre = "Action";
+  print(
+    "========Movie========\n"
+    "Title : $title\n"
+    "Year : $year\n"
+    "Rate : $rate\n"
+    "Genre : $genre\n"
+    "=====================",
+  );
+}
