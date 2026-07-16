99d6192d382daa383e59ab73a678042ea3fd329f
Khaled Waled <khaledwaled2003830@gmail.com>
2026-07-16 04:32:21 +0300

    task3

---
+++ bin/tasks_session1/task3.dart
@@ -0,0 +1,14 @@
+void main() {
+  double math = 90;
+  double science = 80;
+  double english = 40;
+  double total = math + science + english;
+  double average = total / 3;
+  print(
+    "Math = $math\n"
+    "science = $science\n"
+    "English = $english\n"
+    "Total = $total\n"
+    "Average = $average",
+  );
+}
